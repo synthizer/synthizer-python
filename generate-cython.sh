8fd9e5e2049529ffb0b4bd26c98218d454aed633
Austin Hicks <ahicks@ahicks.io>
2021-09-19 12:42:38 -0700

    Fix generate-cython.sh

--- generate-cython.sh
+++ generate-cython.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-# This generates cython .pxd files in bindings/python/synthizer.
+# This generates cython .pxd definitions.
 # To use, pip install autopxd2 in  virtualenv then run the script.
 # We don't run this as part of CI or the build process.
 # You can ignore pragma once in main file warnings.
 
-cd include
-autopxd synthizer.h > ../bindings/python/synthizer/synthizer.pxd
-autopxd synthizer_constants.h > ../bindings/python/synthizer/synthizer_constants.pxd
+cd synthizer-c/include
+autopxd synthizer.h > ../../synthizer/synthizer.pxd
+autopxd synthizer_constants.h > ../../synthizer/synthizer_constants.pxd
