55857b2eaca0158f010a5b922b52fe291b2f697e
Thiago Seus <thiago.seus@yahoo.com.br>
2023-05-18 22:18:25 -0300

    Convert the child FastSineBankGenerator to static methods

--- examples/fast_sine_bank.py
+++ examples/fast_sine_bank.py
@@ -16,25 +16,25 @@ with synthizer.initialized(
     source = synthizer.DirectSource(ctx)
 
     print("Sine")
-    generator = synthizer.FastSineBankGeneratorSine(ctx, 300.0)
+    generator = synthizer.FastSineBankGenerator.create_sine_wave_generator(ctx, 300.0)
     source.add_generator(generator)
     drive_generator(generator)
     generator.dec_ref()
 
     print("Square partials=10")
-    generator = synthizer.FastSineBankGeneratorSquare(ctx, 300.0, 10.0)
+    generator = synthizer.FastSineBankGenerator.create_square_wave_generator(ctx, 300.0, 10.0)
     source.add_generator(generator)
     drive_generator(generator)
     generator.dec_ref()
 
     print("triangle partials=10")
-    generator = synthizer.FastSineBankGeneratorTriangle(ctx, 300.0, 10.0)
+    generator = synthizer.FastSineBankGenerator.create_triangle_wave_generator(ctx, 300.0, 10.0)
     source.add_generator(generator)
     drive_generator(generator)
     generator.dec_ref()
 
     print("sawtooth partials=30")
-    generator = synthizer.FastSineBankGeneratorSaw(ctx, 300.0, 10.0)
+    generator = synthizer.FastSineBankGenerator.create_sawtooth_wave_generator(ctx, 300.0, 10.0)
     source.add_generator(generator)
     drive_generator(generator)
 
